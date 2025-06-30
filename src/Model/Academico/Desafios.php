302be7a4438e3f14a658126a583aca49d0d1538c
RMgit69 <rafamelo916@gmail.com>
2025-06-30 19:58:31 -0300

    Alterações no gerenciamento de torneios

---
+++ src/Model/Academico/Desafios.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Model\Academico;
+use DateTime;
+
+final class Desafios
+{
+    private string $tipo;
+    private string $regras;
+    private DateTime $dataInicio;
+    private DateTime $dataFim;
+    private string $local;
+    
+}
